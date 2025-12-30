6ae1c7bb7ba73a5bbc63dd769b48066c8bfe3fbd
nibirnafis <nibirnafis@gmail.com>
2025-12-31 02:05:30 +0600

    fixed assests file path

--- src/components/TechData.jsx
+++ src/components/TechData.jsx
@@ -7,7 +7,7 @@ const TechData = ({tech}) => {
     return (
         <>
         <div className='flex justify-start items-center gap-2 md:gap-4'>
-            <img className='w-6' src={`/src/assets/icons/${icon}.png`} alt="" />
+            <img className='w-6' src={`/assets/icons/${icon}.png`} alt="" />
             <p className='text-left text-exsml md:text-sml tracking-wide'>{name}</p>
         </div>
         </>
