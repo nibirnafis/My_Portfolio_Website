ef3e16d17bf37779ccaec4ce113753830c0bd11e
nibirnafis <nibirnafis@gmail.com>
2025-12-31 01:54:23 +0600

    fixed naming issues

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router";
 import Icons from "./Icons";
-import SmallButton from "./smallButton";
+import SmallButton from "./SmallButton";
 
 const Project = ({project}) => {
 
