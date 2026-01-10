57456af2960a50fc0fea9ffc6a2562347b73424f
nibirnafis <nibirnafis@gmail.com>
2026-01-10 15:06:21 +0600

    fixed experice component styles

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,22 +11,21 @@ const Experience = ({experience}) => {
                 <div className='overflow-clip max-w-[128px] rounded-[13.6px]'>
                     <img src={img_url} alt="" />
                 </div>
-                <div className='flex flex-col justify-between grow my-4'>
-                    <div className='flex justify-between'>
+                <div className='flex flex-col justify-between grow my-2 md:my-4'>
+                    <div className='flex flex-col gap-2 md:flex-row justify-between'>
                         <div>
                             <p className='mb-1 text-sml md:text-med font-bold '>{company}</p>
                             <p className='text-exsml font-light md:text-sml'>{type}</p>
-                            {/* <p className='mb-2 text-exsml md:text-sml '>{post}{timeline}</p> */}
                         </div>
-                        <div className="hidden md:flex gap-4">
+                        <div className="flex flex-row gap-2 md:gap-4">
                             {
                                 skills.map((icon, index) => <Icons icon={icon} key={index}></Icons>)
                             }
                         </div>
                     </div>
                     <div className='flex flex-col gap-1'>
-                        <p className='text-exsml md:text-sml'>{post.toUpperCase()}</p>
-                        <p className='text-exsml'>({timeline})</p>
+                        <p className='font-light text-exsml md:text-sml'>{post.toUpperCase()}</p>
+                        <p className='font-light text-exsml'>({timeline})</p>
                     </div>
                 </div>
             </div>
